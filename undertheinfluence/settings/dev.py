02af596c983cc5d7b766555ab235be368db0b5ed
Andy Lulham <a.lulham@gmail.com>
2015-09-30 17:59:43 +0100

    Some `manage.py check` recommendations

--- undertheinfluence/settings/dev.py
+++ undertheinfluence/settings/dev.py
@@ -3,7 +3,6 @@ from .base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-TEMPLATE_DEBUG = True
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '5jg+yc7e(k%kz)e+gtk1yzhaz&2b+pgv3c8@==^)h(xhx$w$f_'
