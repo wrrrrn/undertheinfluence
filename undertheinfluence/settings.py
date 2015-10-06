c004b0e2cd16fbe4167c9bcb91bc3be60a9b2425
Andy Lulham <a.lulham@gmail.com>
2015-10-06 12:07:43 +0100

    Fix the settings setup once and for all

--- undertheinfluence/settings.py
+++ undertheinfluence/settings.py
@@ -11,11 +11,20 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 """
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-import os
+from os.path import dirname, exists, join, realpath
+import yaml
 
-PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-BASE_DIR = os.path.dirname(PROJECT_DIR)
+BASE_DIR = realpath(dirname(dirname(__file__)))
+PROJECT_DIR = join(BASE_DIR, 'undertheinfluence')
 
+configuration_file = join(
+    BASE_DIR, 'conf', 'general.yml'
+)
+
+with open(configuration_file) as f:
+    conf = yaml.load(f)
+
+ALLOWED_HOSTS = conf.get('ALLOWED_HOSTS')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
@@ -71,13 +80,19 @@ MIDDLEWARE_CLASSES = (
     'wagtail.wagtailredirects.middleware.RedirectMiddleware',
 )
 
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = conf['SECRET_KEY']
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = bool(int(conf.get('STAGING')))
+
 ROOT_URLCONF = 'undertheinfluence.urls'
 
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(PROJECT_DIR, 'templates'),
+            join(PROJECT_DIR, 'templates'),
         ],
         'APP_DIRS': True,
         'OPTIONS': {
@@ -95,17 +110,39 @@ BOOTSTRAP_ADMIN_SIDEBAR_MENU = True
 
 WSGI_APPLICATION = 'undertheinfluence.wsgi.application'
 
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+# The email address that will be displayed on the site as the contact
+# email for all support requests, and so on:
+SUPPORT_EMAIL = conf.get('SUPPORT_EMAIL')
+
+# The From = address for error emails
+SERVER_EMAIL = conf.get('SERVER_EMAIL')
+
+# The From = address for all emails except error emails
+DEFAULT_FROM_EMAIL = conf.get('DEFAULT_FROM_EMAIL')
 
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if conf.get('DATABASE_SYSTEM') == 'postgresql':
+    DATABASES = {
+        'default': {
+            'ENGINE':   'django.db.backends.postgresql_psycopg2',
+            'NAME':     conf.get('UTI_DB_NAME'),
+            'USER':     conf.get('UTI_DB_USER'),
+            'PASSWORD': conf.get('UTI_DB_PASS'),
+            'HOST':     conf.get('UTI_DB_HOST'),
+            'PORT':     conf.get('UTI_DB_PORT'),
+        }
     }
-}
-
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
@@ -131,15 +168,14 @@ STATICFILES_FINDERS = (
 )
 
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_DIR, 'static'),
+    join(PROJECT_DIR, 'static'),
 )
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
-
 
 # Wagtail settings
 
@@ -150,11 +186,19 @@ SECURE_BROWSER_XSS_FILTER = True
 X_FRAME_OPTIONS = "SAMEORIGIN"  # for the wagtail toolbar
 CSRF_COOKIE_HTTPONLY = True
 
+# Django REST Framework settings
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
     ),
+
     'PAGE_SIZE': 10,
 }
+
+# TheyWorkForYou API key
+TWFY_API_KEY = conf.get('TWFY_API_KEY')
+
+# Email addresses that error emails are sent to when DEBUG = False
+ADMINS = conf['ADMINS']
