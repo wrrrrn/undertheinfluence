26a86000feb348728bffcf865d7db4accde4e4cd
Andy Lulham <a.lulham@gmail.com>
2015-12-02 09:27:18 +0000

    auto-format the *underthe*__influence__ text

--- undertheinfluence/static/uti/js/main.js
+++ undertheinfluence/static/uti/js/main.js
@@ -1,11 +1,14 @@
 $(function() {
-    externalLinks = function(el) {
+    externalLinks = function(doc) {
         // Add an icon to illustrate a link is an external link
-        $('a', el).filter(function() {
+        $('a', doc).filter(function() {
             var isExternal = this.hostname && this.hostname !== location.hostname;
             var hasIcon = $('.glyphicon-share', this).length;
             return isExternal && !hasIcon;
         }).append(' <span class="glyphicon glyphicon-share" aria-hidden="true"></span>');
     }
+
+    document.body.innerHTML = document.body.innerHTML.replace(/undertheInfluence/g, "<em>underthe</em><strong>Influence</strong>");
+
     externalLinks(document);
 });
